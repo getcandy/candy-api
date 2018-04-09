dbbee066fe2a1c4264f3b78857b46b40f334b231
Alec Ritson <alec@neondigital.co.uk>
2018-04-09 11:16:17 +0100

    Add provider

---
+++ src/Providers/PluginServiceProvider.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace GetCandy\Api\Providers;
+
+use File;
+use Illuminate\Support\ServiceProvider;
+
+class PluginServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $loader = require base_path() . '/vendor/autoload.php';
+
+        $list = File::directories(base_path('plugins'));
+
+        foreach ($list as $dir) {
+            $config = require($dir . '/candy.php');
+
+            $namespace = "GetCandy\\Plugins\\" . $config['namespace_suffix'] . "\\";
+
+            $loader->setPsr4($namespace, $dir . "/src/");
+
+            $serviceProvider = $namespace . $config['service_provider'];
+
+            $this->app->register($serviceProvider);
+        }
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
