bc440ff8fcf183d1c835ecba62a46cd9c3008b00
Alec Ritson <alec@neondigital.co.uk>
2018-04-23 17:08:00 +0100

    Payment processing tests

--- tests/Unit/Orders/OrderTest.php
+++ tests/Unit/Orders/OrderTest.php
@@ -8,6 +8,7 @@ use GetCandy\Api\Core\Taxes\Models\Tax;
 use GetCandy\Api\Core\Channels\Models\Channel;
 use GetCandy\Api\Core\Products\Models\Product;
 use GetCandy\Api\Core\Shipping\Models\ShippingPrice;
+use GetCandy\Api\Core\Orders\Exceptions\OrderAlreadyProcessedException;
 
 class OrderTest extends TestCase
 {
@@ -152,10 +153,7 @@ class OrderTest extends TestCase
         }
 
         // Subtotal, manually calculated
-        $calculatedTotal = $order->subTotal - ($order->subTotal * ($percentage / 100));
-        $calculatedTotal = $calculatedTotal + ($calculatedTotal * ($tax->percentage / 100));
-
-        $this->assertEquals($order->total, $calculatedTotal);
+        $this->assertEquals($order->total, $order->subTotal + $order->tax);
     }
 
     public function testCanMakePayment()
@@ -216,13 +214,89 @@ class OrderTest extends TestCase
 
         $transaction = $order->transactions->where('success', true);
 
-        $this->assertEquals($order->total, $transaction->first()->amount);
-        $this->assertInstanceOf(\Carbon\Carbon::class, $order->placed_at);
-        $this->assertEquals('payment-processing', $order->status);
+        if ($transaction->count()) {
+            // If successful
+            $this->assertEquals($order->total, $transaction->first()->amount);
+            $this->assertInstanceOf(\Carbon\Carbon::class, $order->placed_at);
+            $this->assertEquals('payment-processing', $order->status);
 
-        $this->assertTrue(
-            $transaction->count() == 1
-        );
+            $this->assertTrue(
+                $transaction->count() == 1
+            );
+
+        } else {
+            // If not
+            $this->assertNull($order->placed_at);
+            $this->assertTrue(
+                $order->transactions->where('success', false)->count() == 1
+            );
+            $this->assertFalse(
+                $order->transactions->where('success', true)->count()
+            );
+        }
+    }
+
+    public function testProcessSubmitOrderTwice()
+    {
+        $basket = app('api')->baskets()->getBasket();
+
+        // Create our tax bracket
+        $tax = Tax::first();
+        // Get a variant
+        $variant = Product::first()->variants()->first();
+
+        // Make sure our variant is assigned to this tax bracket
+        $variant->tax_id = $tax->id;
+        $variant->save();
+
+        $basket = app('api')->baskets()->store([
+            'basket_id' => $basket->encodedId(),
+            'variants' => [
+                ['id' => $variant->encodedId(), 'quantity' => 2]
+            ]
+        ]);
+
+        app('api')->baskets()->setTotals($basket);
+
+        $order = app('api')->orders()->store($basket->encodedId());
+
+        $price = ShippingPrice::first();
+
+        $order = app('api')->orders()->addShippingLine($order->encodedId(), $price->encodedId());
+
+        $order = app('api')->orders()->setShipping($order->encodedId(), [
+            'phone' => 12345,
+            'firstname' => 'Joe',
+            'lastname' => 'Bloggs',
+            'address' => '123 Some road',
+            'address_two' => 'Somewhere',
+            'city' => 'Some City',
+            'country' => 'United Kingdom',
+            'zip' => 'ZIP123'
+        ]);
+
+        $order = app('api')->orders()->setBilling($order->encodedId(), [
+            'phone' => 12345,
+            'firstname' => 'Joe',
+            'lastname' => 'Bloggs',
+            'address' => '123 Some road',
+            'address_two' => 'Somewhere',
+            'city' => 'Some City',
+            'country' => 'United Kingdom',
+            'zip' => 'ZIP123'
+        ]);
+
+        $order = app('api')->orders()->process([
+            'order_id' => $order->encodedId(),
+            'payment_token' => 'fake-valid-nonce',
+        ]);
+
+        $this->expectException(OrderAlreadyProcessedException::class);
+
+        $dupeResult = app('api')->orders()->process([
+            'order_id' => $order->encodedId(),
+            'payment_token' => 'fake-valid-nonce',
+        ]);
     }
 
     protected function getTaxForAmount($tax, $amount)
