d1ff25f6e07910a8e5a1a25ae3b293986a14c57a
Alec Ritson <alec@neondigital.co.uk>
2018-04-24 10:55:07 +0100

    Update tests/seeders

--- database/seeds/ProductTableSeeder.php
+++ database/seeds/ProductTableSeeder.php
@@ -23,6 +23,7 @@ class ProductTableSeeder extends Seeder
     public function run()
     {
         $language = Language::first()->id;
+        $tax = app('api')->taxes()->getDefaultRecord();
 
         $basic = Layout::create([
             'name' => 'Basic product',
@@ -195,5 +196,157 @@ class ProductTableSeeder extends Seeder
             ]);
         }
 
+        foreach ($product->variants as $variant) {
+            $variant->tax_id = $tax->id;
+            $variant->save();
+        }
+
+        $data = [
+            'layout' => $basic,
+            'option_data' => [
+                'size' => [
+                    'position' => 2,
+                    'label' => [
+                        'en' => 'Size',
+                        'sv' => 'Storlek'
+                    ],
+                    'options' => [
+                        '12' => [
+                            'position' => 2,
+                            'values' => [
+                                'en' => '12',
+                                'sv' => '13',
+                            ]
+                        ],
+                        '10' => [
+                            'position' => 1,
+                            'values' => [
+                                'en' => '10',
+                                'sv' => '11'
+                            ]
+                        ]
+                    ]
+                ],
+                'colour' => [
+                    'position' => 1,
+                    'label' => [
+                        'en' => 'Colour',
+                        'sv' => 'Färg'
+                    ],
+                    'options' => [
+                        'black' => [
+                            'position' => 1,
+                            'values' => [
+                                'en' => 'Black',
+                                'sv' => 'Svart'
+                            ]
+                        ],
+                        'brown' => [
+                            'position' => 2,
+                            'values' => [
+                                'en' => 'Brown',
+                                'sv' => 'Brun'
+                            ]
+                        ],
+
+                    ]
+                ]
+            ],
+            'variants' => [
+                [
+                    'sku' => '335522',
+                    'price' => 30,
+                    'stock' => 1,
+                    'options' => [
+                        'size' => '12',
+                        'colour' => 'brown'
+                    ]
+                ],
+                [
+                    'sku' => '102020',
+                    'price' => 25,
+                    'stock' => 1,
+                    'options' => [
+                        'size' => '10',
+                        'colour' => 'brown'
+                    ]
+                ]
+            ],
+            'attribute_data' => [
+                'name' => [
+                    'en' => 'Leather Boots',
+                    'sv' => 'Läder Buts'
+                ],
+                'material' => [
+                    'en' => 'Leather',
+                    'sv' => 'Läder'
+                ],
+                'description' => [
+                    'en' => 'Legendary lightweight boots made by Blundstone in Tasmania since 1932. Their iconic soles have been engineered for optimum comfort, shock absorption and all-weathers.',
+                    'sv' => 'Legendariska lätta stövlar gjorda av Blundstone i Tasmanien sedan 1932. Deras ikoniska sålar har konstruerats för optimal komfort, stötdämpning och alla väder.'
+                ]
+            ]
+        ];
+
+        $attributes = Attribute::get();
+
+        $fake = \Faker\Factory::create();
+
+        $family = ProductFamily::find(1);
+
+        $product = Product::create([
+            'attribute_data' => $data['attribute_data'],
+            'option_data' => (!empty($data['option_data']) ? $data['option_data'] : [])
+        ]);
+
+        $groups = CustomerGroup::all();
+        $channel = Channel::find(1);
+
+        foreach ($groups as $group) {
+            $product->customerGroups()->attach($group->id, [
+                'visible' => 1,
+                'purchasable' => 1
+            ]);
+        }
+
+        $product->channels()->attach($channel->id, [
+            'published_at' => Carbon::now(),
+        ]);
+
+        foreach ($attributes as $att) {
+            $product->attributes()->attach($att);
+        }
+
+        $product->layout()->associate($data['layout']);
+        $product->family()->associate($family);
+        foreach ($product->attribute_data['name'] as $channel => $attr_data) {
+            if ($channel == 'ecommerce') {
+                $product->route()->create([
+                    'default' => true,
+                    'slug' => str_slug($attr_data['en']),
+                    'locale' => 'en'
+                ]);
+            }
+        }
+
+        $product->save();
+
+        if (!empty($data['variants'])) {
+            foreach ($data['variants'] as $variant) {
+                $product->variants()->create($variant);
+            }
+        } else {
+            $product->variants()->create([
+                'options' => [],
+                'sku' => 'LTHRBTS',
+                'stock' => 1,
+                'price' => 25
+            ]);
+        }
+
+        foreach ($product->variants as $variant) {
+            $variant->tax_id = $tax->id;
+            $variant->save();
+        }
     }
 }
--- tests/Unit/Orders/OrderTest.php
+++ tests/Unit/Orders/OrderTest.php
@@ -2,8 +2,11 @@
 
 namespace Tests\Unit;
 
+use TaxCalculator;
 use Tests\TestCase;
+use PriceCalculator;
 use Tests\Stubs\User;
+use CurrencyConverter;
 use GetCandy\Api\Core\Taxes\Models\Tax;
 use GetCandy\Api\Core\Channels\Models\Channel;
 use GetCandy\Api\Core\Products\Models\Product;
@@ -13,6 +16,11 @@ use GetCandy\Api\Core\Orders\Exceptions\OrderAlreadyProcessedException;
 class OrderTest extends TestCase
 {
 
+    /**
+     * Tests if a guest order can be created from a basket
+     *
+     * @return void
+     */
     public function testOrderIsCreatedFromBasket()
     {
         $basket = app('api')->baskets()->getBasket();
@@ -53,7 +61,44 @@ class OrderTest extends TestCase
             $this->assertTrue($line->tax == $this->getTaxForAmount($tax, $line->line_amount));
             $this->assertTrue(!$line->discount);
         }
+    }
 
+    public function testCanAddMultipleItemsToOrder()
+    {
+        $basket = app('api')->baskets()->getBasket();
+
+        // Create our tax bracket
+        $tax = app('api')->taxes()->getDefaultRecord();
+
+        // Get a variant
+        $variantOne = Product::first()->variants()->first();
+        $variantTwo = Product::find(2)->variants()->first();
+
+        $basket = app('api')->baskets()->store([
+            'basket_id' => $basket->encodedId(),
+            'variants' => [
+                ['id' => $variantOne->encodedId(), 'quantity' => 1],
+                ['id' => $variantTwo->encodedId(), 'quantity' => 1]
+            ]
+        ]);
+
+        app('api')->baskets()->setTotals($basket);
+
+
+        $taxAmount = $this->getTaxForAmount($tax, $variantOne->price + $variantTwo->price);
+
+        $this->assertTrue($basket->tax == $taxAmount);
+
+        $this->assertEquals($basket->total, $variantOne->price + $variantTwo->price);
+
+        $order = app('api')->orders()->store($basket->encodedId());
+
+        $this->assertTrue($order->lines->count() == 2);
+
+        foreach ($order->lines as $line) {
+            $this->assertEquals($line->tax, $this->getTaxForAmount($tax, $line->line_amount));
+            $this->assertTrue(!$line->discount);
+        }
     }
 
     public function testCanAddShippingToOrder()
@@ -145,7 +190,8 @@ class OrderTest extends TestCase
         foreach ($order->lines as $line) {
             if (!$line->shipping) {
                 $this->assertEquals(
-                    $line->line_amount * ($percentage / 100), $line->discount
+                    $line->line_amount * ($percentage / 100),
+                    $line->discount
                 );
             } else {
                 $this->assertTrue(!$line->discount);
@@ -223,7 +269,6 @@ class OrderTest extends TestCase
             $this->assertTrue(
                 $transaction->count() == 1
             );
-
         } else {
             // If not
             $this->assertNull($order->placed_at);
@@ -231,12 +276,12 @@ class OrderTest extends TestCase
                 $order->transactions->where('success', false)->count() == 1
             );
             $this->assertFalse(
-                $order->transactions->where('success', true)->count()
+                $order->transactions->where('success', true)->count() == 1
             );
         }
     }
 
-    public function testProcessSubmitOrderTwice()
+    public function testCantProcessOrderTwice()
     {
         $basket = app('api')->baskets()->getBasket();
 
@@ -297,6 +342,76 @@ class OrderTest extends TestCase
             'order_id' => $order->encodedId(),
             'payment_token' => 'fake-valid-nonce',
         ]);
+    }
+
+    public function testCanGetOrderWithoutTax()
+    {
+        TaxCalculator::setTax();
+
+        $basket = app('api')->baskets()->getBasket();
+
+        $firstBasketId = $basket->id;
+
+        // Create our tax bracket
+        $tax = app('api')->taxes()->getDefaultRecord();
+
+        // Get a variant
+        $variant = Product::first()->variants()->first();
+
+        // Make sure our variant is assigned to this tax bracket
+        $variant->tax_id = $tax->id;
+        $variant->save();
+
+        $basket = app('api')->baskets()->store([
+            'basket_id' => $basket->encodedId(),
+            'variants' => [
+                ['id' => $variant->encodedId(), 'quantity' => 1]
+            ]
+        ]);
+
+        app('api')->baskets()->setTotals($basket);
+
+        // Make an order
+        $order = app('api')->orders()->store($basket->encodedId());
+
+        // Make sure shipping has no tax added too.
+        $price = ShippingPrice::first();
+        $order = app('api')->orders()->addShippingLine($order->encodedId(), $price->encodedId());
+
+        $this->assertTrue($order->lines->count() == 2);
+
+        $shipping = $order->lines->where('shipping', 1)->first();
+
+        $this->assertEquals($price->rate, $shipping->line_amount + $shipping->tax);
+
+        $this->assertEquals($order->subTotal, $order->total);
+    }
+
+    public function testCanAddEuroOrder()
+    {
+        CurrencyConverter::set('EUR');
+
+        $basket = app('api')->baskets()->getBasket();
+        $currency = app('api')->currencies()->getByCode('EUR');
+
+        // Create our tax bracket
+        $tax = app('api')->taxes()->getDefaultRecord();
+
+        // Get a variant
+        $variant = Product::first()->variants()->first();
+
+        $basket = app('api')->baskets()->store([
+            'basket_id' => $basket->encodedId(),
+            'variants' => [
+                ['id' => $variant->encodedId(), 'quantity' => 1]
+            ]
+        ]);
+
+        app('api')->baskets()->setTotals($basket);
+
+        $order = app('api')->orders()->store($basket->encodedId());
+
+        $this->assertEquals($order->subTotal, round($variant->price / $currency->exchange_rate, 2));
     }
 
     protected function getTaxForAmount($tax, $amount)
