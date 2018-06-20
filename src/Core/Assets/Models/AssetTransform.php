592faa353e25bfc073ef7d6eb96da3721a0bb1f7
Alec Ritson <alec@neondigital.co.uk>
2018-06-20 15:52:23 +0100

    Include transforms and eager load

--- src/Core/Assets/Models/AssetTransform.php
+++ src/Core/Assets/Models/AssetTransform.php
@@ -3,12 +3,20 @@
 namespace GetCandy\Api\Core\Assets\Models;
 
 use GetCandy\Api\Core\Scaffold\BaseModel;
+use Storage;
 
 class AssetTransform extends BaseModel
 {
     protected $hashids = 'main';
 
     protected $table = 'asset_transforms';
+
+    public function toArray()
+    {
+        return array_merge(parent::toArray(), [
+            'url' => Storage::disk($this->asset->source->disk)->url($this->location . '/' . $this->filename)
+        ]);
+    }
 
     public function transform()
     {
