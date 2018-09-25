f6e65065e88de09a377a3b0b373c30accbb82457
Alec Ritson <alec@neondigital.co.uk>
2018-09-25 16:46:40 +0100

    Get refunds up and running

--- src/Core/Payments/Providers/SagePay.php
+++ src/Core/Payments/Providers/SagePay.php
@@ -27,6 +27,41 @@ class SagePay extends AbstractProvider
         return true;
     }
 
+    public function refund($token, $amount, $description)
+    {
+        $client = new Client([
+            'base_uri' => $this->host,
+        ]);
+
+        try {
+            $payload = [
+                'transactionType' => 'Refund',
+                'amount' => $amount,
+                'referenceTransactionId' => $token,
+                'currency' => $this->order->currency, // Get currency from order.
+                'vendorTxCode' => str_random(40),
+                "description" => $description,
+            ];
+
+            $response = $client->request('POST', 'transactions', [
+                'headers' => [
+                    'Authorization' => 'Basic '.$this->getCredentials(),
+                    'Content-Type' => 'application/json',
+                    'Cache-Control' => 'no-cache',
+                ],
+                'json' => $payload,
+            ]);
+        } catch (ClientException $e) {
+            $errors = json_decode($e->getResponse()->getBody()->getContents(), true);
+            $response = new PaymentResponse(false, 'Refund Failed', $errors);
+            return $this->createFailedTransaction($errors);
+        }
+
+        $content = json_decode($response->getBody()->getContents(), true);
+
+        return $this->createRefundTransaction($content, $amount, $description);
+    }
+
     public function charge()
     {
         $client = new Client([
@@ -102,6 +137,34 @@ class SagePay extends AbstractProvider
         return base64_encode($order->encodedId().'-'.microtime(true));
     }
 
+    protected function getBaseTransaction($content)
+    {
+        $transaction = new Transaction;
+        $transaction->order()->associate($this->order);
+        $transaction->merchant = $this->getVendor();
+        $transaction->provider = 'SagePay';
+        $transaction->driver = 'sagepay';
+
+        return $transaction;
+    }
+
+    protected function createRefundTransaction($content, $amount, $notes)
+    {
+        $transaction = $this->getBaseTransaction($content);
+        $transaction->amount = -abs($amount);
+        $transaction->card_type = $content['paymentMethod']['card']['cardType'] ?? 'Unknown';
+        $transaction->last_four = $content['paymentMethod']['card']['lastFourDigits'] ?? '';
+        $transaction->transaction_id = $content['transactionId'];
+        $transaction->driver = 'sagepay';
+        $transaction->status = $content['statusDetail'];
+        $transaction->success = true;
+        $transaction->notes = $notes;
+        $transaction->refund = true;
+        $transaction->save();
+
+        return $transaction;
+    }
+
     protected function createSuccessTransaction($content, $order)
     {
         $transaction = new Transaction;
@@ -110,6 +173,7 @@ class SagePay extends AbstractProvider
         $transaction->order()->associate($order);
         $transaction->merchant = $this->getVendor();
         $transaction->provider = 'SagePay';
+        $transaction->driver = 'sagepay';
         $transaction->status = $content['status'];
         $transaction->amount = $content['amount']['totalAmount'];
         $transaction->card_type = $content['paymentMethod']['card']['cardType'] ?? 'Unknown';
@@ -124,6 +188,12 @@ class SagePay extends AbstractProvider
         return $transaction;
     }
 
+    /**
+     * Create a failed transaction
+     *
+     * @param array $errors
+     * @return Transaction
+     */
     protected function createFailedTransaction($errors)
     {
         $transaction = new Transaction;
@@ -142,10 +212,11 @@ class SagePay extends AbstractProvider
         return $transaction;
     }
 
-    public function refund($token, $amount = null)
-    {
-    }
-
+    /**
+     * Get the client token for authentication
+     *
+     * @return void
+     */
     public function getClientToken()
     {
         $client = new Client([
@@ -176,16 +247,31 @@ class SagePay extends AbstractProvider
         return $response['merchantSessionKey'] ?? null;
     }
 
+    /**
+     * Get the token expiry
+     *
+     * @return Carbon\Carbon
+     */
     public function getTokenExpiry()
     {
         return Carbon::parse($this->tokenExpires);
     }
 
+    /**
+     * Get the vendor name
+     *
+     * @return string
+     */
     protected function getVendor()
     {
         return config('services.sagepay.vendor');
     }
 
+    /**
+     * Get the service credentials
+     *
+     * @return string
+     */
     protected function getCredentials()
     {
         return base64_encode(config('services.sagepay.key').':'.config('services.sagepay.password'));
