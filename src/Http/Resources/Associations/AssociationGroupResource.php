1bbc0a8bf2ef540133d028288e9788234226a39c
Alec Ritson <hello@itsalec.co.uk>
2018-12-04 12:19:26 +0000

    Add new resource

---
+++ src/Http/Resources/Associations/AssociationGroupResource.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace GetCandy\Api\Http\Resources\Associations;
+
+use GetCandy\Api\Http\Resources\AbstractResource;
+
+class AssociationGroupResource extends AbstractResource
+{
+    public function payload()
+    {
+        return [
+            'id' => $this->encoded_id,
+        ];
+    }
+}
