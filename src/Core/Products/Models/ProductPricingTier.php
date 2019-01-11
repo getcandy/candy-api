ff3d5268cdd840e3a9ede3994af98c1920eac767
Alec Ritson <hello@itsalec.co.uk>
2019-01-11 11:19:24 +0000

    Add scope for customer group

--- src/Core/Products/Models/ProductPricingTier.php
+++ src/Core/Products/Models/ProductPricingTier.php
@@ -5,6 +5,7 @@ namespace GetCandy\Api\Core\Products\Models;
 use PriceCalculator;
 use GetCandy\Api\Core\Scaffold\BaseModel;
 use GetCandy\Api\Core\Customers\Models\CustomerGroup;
+use Illuminate\Database\Eloquent\Builder;
 
 class ProductPricingTier extends BaseModel
 {
@@ -14,6 +15,34 @@ class ProductPricingTier extends BaseModel
         'lower_limit',
         'price',
     ];
+
+        /**
+     * The "booting" method of the model.
+     *
+     * @return void
+     */
+    protected static function boot()
+    {
+        parent::boot();
+
+        $roles = app('api')->roles()->getHubAccessRoles();
+
+        if ($user = app('auth')->user()) {
+            $groups = $user->groups->pluck('id')->toArray();
+        } else {
+            $groups = [app('api')->customerGroups()->getGuestId()];
+        }
+
+        $user = app('auth')->user();
+
+        static::addGlobalScope('available', function (Builder $builder) use ($user, $groups, $roles) {
+            if (! $user || ! $user->hasAnyRole($roles)) {
+                $builder->whereHas('group', function ($query) use ($groups) {
+                    $query->whereIn('id', $groups);
+                });
+            }
+        });
+    }
 
     public function scopeInGroups($query, $groups)
     {
@@ -56,5 +85,16 @@ class ProductPricingTier extends BaseModel
     public function group()
     {
         return $this->belongsTo(CustomerGroup::class, 'customer_group_id');
+    }
+
+
+    protected function getCustomerGroups()
+    {
+        // If there is a user, get their groups.
+        if ($user = app('auth')->user()) {
+            return $user->groups->pluck('id')->toArray();
+        } else {
+            return [app('api')->customerGroups()->getGuestId()];
+        }
     }
 }
