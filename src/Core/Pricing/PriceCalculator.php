fab835efe764a0364d091eb85cb0086e1fbb8519
Alec Ritson <hello@itsalec.co.uk>
2019-02-01 12:16:29 +0000

    Return a new object

--- src/Core/Pricing/PriceCalculator.php
+++ src/Core/Pricing/PriceCalculator.php
@@ -7,8 +7,6 @@ use GetCandy\Api\Core\Taxes\Interfaces\TaxCalculatorInterface;
 
 class PriceCalculator implements PriceCalculatorInterface
 {
-    protected $pricing = [];
-
     /**
      * The currency converter instance
      *
@@ -29,14 +27,6 @@ class PriceCalculator implements PriceCalculatorInterface
         $this->taxes = $taxes;
     }
 
-    public function __get($property)
-    {
-        if (isset($this->pricing[$property])) {
-            return $this->pricing[$property];
-        }
-        throw new InvalidArgumentException("Method or Property {$property} doesn't exist");
-    }
-
     public function get($price, $tax = 0, $qty = 1, $factor = 1)
     {
         $unitPrice = $price / $factor;
@@ -51,17 +41,15 @@ class PriceCalculator implements PriceCalculatorInterface
             $factorTax = $this->taxes->amount($price);
         }
 
-        $this->pricing = [
-            'base_cost' => $price,
-            'factor_tax' => $factorTax,
-            'unit_cost' => $unitPrice,
-            'unit_tax' => round($taxamount / $qty, 2),
+        return new PriceCalculatorResult([
+            'baseCost' => $price,
+            'factorTax' => $factorTax,
+            'unitCost' => $unitPrice,
+            'unitTax' => round($taxamount / $qty, 2),
             'factor' => $factor,
-            'total_cost' => round($converted, 2),
-            'total_tax' => $taxamount,
+            'totalCost' => round($converted, 2),
+            'totalTax' => $taxamount,
             'qty' => (int) $qty,
-        ];
-
-        return $this;
+        ]);
     }
 }
