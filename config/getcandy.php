d893af319eda6b02bc575dc3561ca48c74349cec
Alec Ritson <hello@itsalec.co.uk>
2019-02-07 09:07:19 +0000

    Add config default

--- config/getcandy.php
+++ config/getcandy.php
@@ -6,6 +6,11 @@ return [
      */
     'hub_access' => ['editor'],
 
+    /**
+     * Define whether to use internal requests
+     */
+    'internal_requests' => env('CANDY_INTERNAL_REQUESTS', false),
+
     /*
      * The URL to your storefront
      */
