0615c8bd88181c1f883f91a88d2b399a6f8ed7e1
Alec Ritson <hello@itsalec.co.uk>
2019-02-12 10:47:19 +0000

    Add interface comment

--- src/Core/ActivityLog/Interfaces/ActivityLogCriteriaInterface.php
+++ src/Core/ActivityLog/Interfaces/ActivityLogCriteriaInterface.php
@@ -5,4 +5,10 @@ namespace GetCandy\Api\Core\ActivityLog\Interfaces;
 interface ActivityLogCriteriaInterface
 {
 
+    /**
+     * Gets the underlying builder for the query.
+     *
+     * @return \Illuminate\Database\Eloquent\QueryBuilder
+     */
+    public function getBuilder();
 }
