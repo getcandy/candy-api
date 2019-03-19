60d1d332d2deb5df7fe4534fa79e7dc67cce04d2
Alec Ritson <hello@itsalec.co.uk>
2019-03-19 14:42:03 +0000

    Include variant images

--- src/Core/Baskets/Services/BasketService.php
+++ src/Core/Baskets/Services/BasketService.php
@@ -91,6 +91,7 @@ class BasketService extends BaseService
             'lines.variant',
             'lines.variant.tax',
             'lines.variant.tiers',
+            'lines.variant.image.transforms',
             'lines.variant.product',
             'lines.variant.product.assets',
             'lines.variant.product.assets.transforms',
@@ -187,7 +188,11 @@ class BasketService extends BaseService
         if (! empty($data['variants'])) {
             $this->remapLines($basket, $data['variants']);
         }
-        $basket->load('lines');
+        $basket->load([
+            'lines',
+            'lines.variant.product.routes',
+            'lines.variant.image.transforms',
+        ]);
 
         $discounts = Discount::all();
 
