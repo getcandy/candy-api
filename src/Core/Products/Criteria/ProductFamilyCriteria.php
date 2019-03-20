9ffcd00750bcec7d41a3ff00f0cc127b892d67fb
Alec Ritson <hello@itsalec.co.uk>
2019-03-20 10:07:53 +0000

    Init

---
+++ src/Core/Products/Criteria/ProductFamilyCriteria.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace GetCandy\Api\Core\Products\Criteria;
+
+use GetCandy\Api\Core\Products\Models\ProductFamily;
+use GetCandy\Api\Core\Scaffold\AbstractCriteria;
+
+class ProductFamilyCriteria extends AbstractCriteria
+{
+    public function getBuilder()
+    {
+        $family = new ProductFamily;
+        $builder = $family->with($this->includes ?: []);
+        if ($this->id) {
+            $builder->where('id', '=', $family->decodeId($this->id));
+            return $builder;
+        }
+        return $builder;
+    }
+}
