23caf4923745adf651931344e917f7f8033e1fe8
Alec Ritson <hello@itsalec.co.uk>
2019-03-27 09:02:26 +0000

    Remove parent as causing issues

--- src/Http/Resources/Categories/CategoryResource.php
+++ src/Http/Resources/Categories/CategoryResource.php
@@ -27,7 +27,6 @@ class CategoryResource extends AbstractResource
     {
         return [
             'children' => new CategoryCollection($this->whenLoaded('children'), $this->only),
-            'parent' => ['data' => new CategoryResource($this->whenLoaded('parent'))],
             'channels' => new ChannelCollection($this->whenLoaded('channels')),
             'ancestors' => new CategoryCollection($this->whenLoaded('ancestors')),
             'routes' => new RouteCollection($this->whenLoaded('routes')),
