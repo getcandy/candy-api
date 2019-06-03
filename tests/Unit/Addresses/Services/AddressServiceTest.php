5307b9665f7a42b7bf8cb32b8896866c6f6a6efa
Jamie Warburton <jamie@hexdigital.com>
2019-06-03 13:08:05 +0100

    Add tests for AddressService default address methods

---
+++ tests/Unit/Addresses/Services/AddressServiceTest.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Tests\Unit\Addresses\Services;
+
+use Carbon\Carbon;
+use Tests\TestCase;
+use Tests\Stubs\User;
+use GetCandy\Api\Core\Addresses\Models\Address;
+use GetCandy\Api\Core\Addresses\Services\AddressService;
+
+/**
+ * @group addresses
+ */
+class AddressServiceTest extends TestCase
+{
+    /** @var AddressService */
+    protected $service;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->service = $this->app->make(AddressService::class);
+    }
+
+    public function test_can_make_address_default()
+    {
+        $address = Address::first();
+
+        $this->assertNotDefaultAddress($address);
+
+        $this->service->makeDefault($address);
+
+        $this->assertDefaultAddress($address);
+    }
+
+    public function test_can_undefault_other_addresses_when_make_address_default()
+    {
+        $originalAddress = Address::first();
+        $newAddress = $originalAddress->replicate();
+        $newAddress->save();
+
+        $originalAddress->is_default = true;
+        $originalAddress->save();
+
+        $this->assertDefaultAddress($originalAddress);
+        $this->assertNotDefaultAddress($newAddress);
+
+        $this->service->makeDefault($newAddress);
+
+        $this->assertNotDefaultAddress($originalAddress);
+        $this->assertDefaultAddress($newAddress);
+    }
+
+    public function test_can_remove_default_from_address()
+    {
+        $address = Address::first();
+        $address->is_default = true;
+        $address->save();
+
+        $this->assertDefaultAddress($address);
+
+        $this->service->removeDefault($address);
+
+        $this->assertNotDefaultAddress($address);
+    }
+
+    private function assertDefaultAddress(Address $address)
+    {
+        $this->assertTrue($address->is_default);
+    }
+
+    private function assertNotDefaultAddress(Address $address)
+    {
+        $this->assertFalse($address->is_default);
+    }
+}
