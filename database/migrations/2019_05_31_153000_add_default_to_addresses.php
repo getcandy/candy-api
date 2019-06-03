8a30f350e03dc76e8fba7a93d6858b02c96e419d
Jamie Warburton <jamie@hexdigital.com>
2019-06-03 15:30:27 +0100

    Change migration name from is default to just default

--- database/migrations/2019_05_31_153000_add_default_to_addresses.php
+++ database/migrations/2019_05_31_153000_add_default_to_addresses.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddIsDefaultToAddresses extends Migration
+class AddDefaultToAddresses extends Migration
 {
     /**
      * Run the migrations.
