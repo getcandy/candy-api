625a2af1d607bdacf18a0cb28aba640db56c7725
Jamie Warburton <jamie@hexdigital.com>
2019-07-12 15:30:58 +0100

    Undo change, as the DB access was required

--- src/Http/Requests/Baskets/CreateLinesRequest.php
+++ src/Http/Requests/Baskets/CreateLinesRequest.php
@@ -28,7 +28,7 @@ class CreateLinesRequest extends FormRequest
             'variants' => 'array|unique_lines',
         ];
 
-        $variants = app('api')->productVariants()->getDecodedIds(
+        $variants = app('api')->productVariants()->getByHashedIds(
             collect($this->variants)->pluck('id')->toArray()
         );
 
