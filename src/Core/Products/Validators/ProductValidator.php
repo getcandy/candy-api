ff0198d1a6148363fd94b308e72797e1b0c305da
Alec Ritson <hello@itsalec.co.uk>
2019-10-18 11:14:06 +0100

    Add validation replacer for error message

--- src/Core/Products/Validators/ProductValidator.php
+++ src/Core/Products/Validators/ProductValidator.php
@@ -4,8 +4,19 @@ namespace GetCandy\Api\Core\Products\Validators;
 
 class ProductValidator
 {
-    public function available($value, $variantId)
+    public function available($attribute, $value, $parameter, $validator)
     {
-        return app('api')->productVariants()->variantIsAvailable($variantId);
+        $variant = app('api')->productVariants()->getByHashedId($value);
+
+        if (!$variant) {
+            return false;
+        }
+
+        $validator->addReplacer('available', function ($message, $attribute, $rule, $parameters) use ($variant) {
+            return trans('getcandy::validation.available', [
+                'sku' => $variant->sku,
+            ]);
+        });
+        return $variant->availableProduct()->exists();
     }
 }
