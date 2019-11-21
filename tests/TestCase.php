d7a85f543c1721cf6daeaf7e24fc9443848a8a8a
Alec Ritson <hello@itsalec.co.uk>
2019-11-21 10:51:25 +0000

    Make sure we actually publish activity log bits

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -37,7 +37,7 @@ abstract class TestCase extends \Orchestra\Testbench\TestCase
         parent::setUp();
 
         $this->artisan('cache:forget', ['key' => 'spatie.permission.cache']);
-        // $this->artisan('vendor:publish', ['--provider' => 'Spatie\Activitylog\ActivitylogServiceProvider', '--tag' => 'migrations']);
+        $this->artisan('vendor:publish', ['--provider' => 'Spatie\Activitylog\ActivitylogServiceProvider', '--tag' => 'migrations']);
         $this->artisan('migrate', ['--database' => 'testing']);
 
         if ($this->withSeedData) {
