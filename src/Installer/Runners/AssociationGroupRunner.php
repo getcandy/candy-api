bfb56cf24024d6038f449acea9dd8353e693b438
Alec Ritson <hello@itsalec.co.uk>
2019-11-21 10:58:51 +0000

    Remove command instance

--- src/Installer/Runners/AssociationGroupRunner.php
+++ src/Installer/Runners/AssociationGroupRunner.php
@@ -4,25 +4,15 @@ namespace GetCandy\Api\Installer\Runners;
 
 use DB;
 use GetCandy\Api\Installer\Contracts\InstallRunnerContract;
-use Illuminate\Console\Command;
 
 class AssociationGroupRunner extends AbstractRunner implements InstallRunnerContract
 {
-    protected $command;
-
-    public function __construct(Command $command)
-    {
-        $this->command = $command;
-    }
-
     public function run()
     {
         // Are languages already installed?
         if (DB::table('association_groups')->count()) {
             return;
         }
-
-        $this->command->info('Installing Association Groups');
 
         DB::table('association_groups')->insert([
             [
