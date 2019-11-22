1f0a17247dcb76a055a5023c40cccae528b8fe9f
Alec Ritson <hello@itsalec.co.uk>
2019-11-22 13:44:32 +0000

    Clean up test cases

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,11 +2,22 @@
 
 namespace Tests;
 
+use Facades\GetCandy\Api\Core\Pricing\PriceCalculator;
+use Facades\GetCandy\Api\Core\Taxes\TaxCalculator;
 use GetCandy\Api\Core\Baskets\Factories\BasketFactory;
+use GetCandy\Api\Core\Baskets\Models\Basket;
+use GetCandy\Api\Core\Baskets\Models\BasketLine;
 use GetCandy\Api\Core\Channels\Interfaces\ChannelFactoryInterface;
 use GetCandy\Api\Core\Channels\Models\Channel;
+use GetCandy\Api\Core\Currencies\Facades\CurrencyConverter;
+use GetCandy\Api\Core\Facades\GetCandyFacade;
+use GetCandy\Api\Core\Products\Models\ProductVariant;
 use GetCandy\Api\Providers\ApiServiceProvider;
+use Laravel\Passport\PassportServiceProvider;
+use Spatie\Activitylog\ActivitylogServiceProvider;
+use Spatie\Permission\PermissionServiceProvider;
 use Tests\Stubs\User;
+use Vinkla\Hashids\HashidsServiceProvider;
 
 abstract class TestCase extends \Orchestra\Testbench\TestCase
 {
@@ -36,6 +47,7 @@ abstract class TestCase extends \Orchestra\Testbench\TestCase
         $this->artisan('cache:forget', ['key' => 'spatie.permission.cache']);
         $this->artisan('migrate', ['--database' => 'testing']);
         $this->artisan('db:seed', ['--class' => '\Seeds\TestingDatabaseSeeder']);
+        $this->withFactories(dirname(__DIR__).'/database/factories');
 
         // Make sure our channel is set.
         $channel = app()->getInstance()->make(ChannelFactoryInterface::class);
@@ -52,55 +64,45 @@ abstract class TestCase extends \Orchestra\Testbench\TestCase
     {
         parent::getEnvironmentSetUp($app);
 
-        $app->useEnvironmentPath(__DIR__.'/..');
+        $config = [
+            'permission' => require __DIR__.'/../vendor/spatie/laravel-permission/config/permission.php',
+            'hashids' => require __DIR__.'/../config/hashids.php',
+            'auth.providers.users.model' => User::class,
+            'services.sagepay.vendor' => 'SagePay',
+            'getcandy' => require __DIR__.'/../config/getcandy.php',
+        ];
 
-        //Blergh but we need the config
-        $app['config']['permission'] = require realpath(__DIR__.'/../vendor/spatie/laravel-permission/config/permission.php');
-        $app['config']['hashids'] = require realpath(__DIR__.'/../config/hashids.php');
-        $app['config']->set('database.default', 'testing');
-        $app['config']->set('database.connections.testing', [
-            'driver' => 'sqlite',
-            'database' => ':memory:',
-            'prefix' => '',
-        ]);
+        foreach ($config as $key => $value) {
+            $app['config']->set($key, $value);
+        }
 
-        $app['config']->set('auth.providers.users.model', User::class);
-
-        $app['config']->set('services', [
-            'sagepay' => [
-                'vendor' => 'SagePay',
-            ],
-        ]);
-
-        // GetCandy specific
-        $app['config']->set('getcandy', require realpath(__DIR__.'/../config/getcandy.php'));
     }
 
     protected function getPackageProviders($app)
     {
         return [
-            \Laravel\Passport\PassportServiceProvider::class,
+            PassportServiceProvider::class,
             ApiServiceProvider::class,
-            \Spatie\Permission\PermissionServiceProvider::class,
-            \Spatie\Activitylog\ActivitylogServiceProvider::class,
-            \Vinkla\Hashids\HashidsServiceProvider::class,
+            PermissionServiceProvider::class,
+            ActivitylogServiceProvider::class,
+            HashidsServiceProvider::class,
         ];
     }
 
     protected function getPackageAliases($app)
     {
         return [
-            'CurrencyConverter' => \GetCandy\Api\Core\Currencies\Facades\CurrencyConverter::class,
-            'TaxCalculator' => \Facades\GetCandy\Api\Core\Taxes\TaxCalculator::class,
-            'PriceCalculator' => \Facades\GetCandy\Api\Core\Pricing\PriceCalculator::class,
-            'GetCandy' => \GetCandy\Api\Core\Facades\GetCandyFacade::class,
+            'CurrencyConverter' => CurrencyConverter::class,
+            'TaxCalculator' => TaxCalculator::class,
+            'PriceCalculator' => PriceCalculator::class,
+            'GetCandy' => GetCandyFacade::class,
         ];
     }
 
     protected function getinitalbasket($user = null)
     {
-        $variant = \GetCandy\Api\Core\Products\Models\ProductVariant::first();
-        $basket = \GetCandy\Api\Core\Baskets\Models\Basket::forceCreate([
+        $variant = ProductVariant::first();
+        $basket = Basket::forceCreate([
             'currency' => 'GBP',
         ]);
 
@@ -109,7 +111,7 @@ abstract class TestCase extends \Orchestra\Testbench\TestCase
             $basket->save();
         }
 
-        \GetCandy\Api\Core\Baskets\Models\BasketLine::forceCreate([
+        BasketLine::forceCreate([
             'product_variant_id' => $variant->id,
             'basket_id' => $basket->id,
             'quantity' => 1,
@@ -118,4 +120,5 @@ abstract class TestCase extends \Orchestra\Testbench\TestCase
 
         return $this->app->make(BasketFactory::class)->init($basket)->get();
     }
+
 }
