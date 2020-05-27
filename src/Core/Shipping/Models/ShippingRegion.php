9b11e14f9cd04706d576b50aacfda6732c31c274
Jesus Baron <jbaron@outlook.com>
2020-05-27 00:37:20 -0500

    Merge master fixes into develop branch (#203)
    * Merge in develop
    
    * Update README.md
    
    * Remove dupe
    
    * Fix dupe lines
    
    Co-authored-by: Alec <hello@itsalec.co.uk>

--- src/Core/Shipping/Models/ShippingRegion.php
+++ src/Core/Shipping/Models/ShippingRegion.php
@@ -3,7 +3,6 @@
 namespace GetCandy\Api\Core\Shipping\Models;
 
 use GetCandy\Api\Core\Countries\Models\Country;
-use GetCandy\Api\Core\Countries\Models\Country;
 use GetCandy\Api\Core\Scaffold\BaseModel;
 
 class ShippingRegion extends BaseModel
