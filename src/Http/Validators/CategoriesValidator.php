cc4e96e595ab6e314fc3979099a3e01e9af7a3b2
Jesus <jbaron@outlook.com>
2020-06-09 18:38:37 -0500

    Fixed all docblocks for Validator classes

--- src/Http/Validators/CategoriesValidator.php
+++ src/Http/Validators/CategoriesValidator.php
@@ -6,8 +6,11 @@ class CategoriesValidator
 {
     /**
      * Validates the name for an attribute doesn't exist in the same group.
-     * @param  string $attribute
-     * @param  string $value
+     * 
+     * @param  string  $attribute
+     * @param  string  $value
+     * @param  array  $key
+     * @param  \Illuminate\Contracts\Validation\Validator  $validator
      * @return bool
      */
     public function uniqueCategoryAttributeData($attribute, $value, $key, $validator)
