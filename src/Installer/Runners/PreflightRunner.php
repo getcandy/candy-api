1fafc9841991b42410aa3f8ec265029a1b025537
Dan Storm <storm@err0r.dk>
2020-06-23 13:58:33 +0200

    Add preflight checks for Elasticsearch

--- src/Installer/Runners/PreflightRunner.php
+++ src/Installer/Runners/PreflightRunner.php
@@ -3,6 +3,8 @@
 namespace GetCandy\Api\Installer\Runners;
 
 use DB;
+use Elastica\Client;
+use Elastica\Exception\Connection\HttpException;
 use GetCandy;
 use GetCandy\Api\Installer\Contracts\InstallRunnerContract;
 use GetCandy\Api\Installer\Events\PreflightCompletedEvent;
@@ -24,6 +26,19 @@ class PreflightRunner extends AbstractRunner implements InstallRunnerContract
             $dbVersion = null;
         }
 
+        try {
+
+            $elastica = new Client(config('getcandy.search.client_config.elastic'));
+
+            $result = $elastica->request('/');
+
+            $esVersion = $result->getData()['version']['number'];
+
+        } catch (HttpException $e) {
+            $esVersion = null;
+        }
+
+
         event(new PreflightCompletedEvent([
             'api' => [
                 'version' => $apiVersion,
@@ -34,6 +49,14 @@ class PreflightRunner extends AbstractRunner implements InstallRunnerContract
                 'connected' => $dbVersion ?? false,
                 'success' => (bool) $dbVersion,
             ],
+            'elasticsearch' => [
+                'version' => $esVersion,
+                'connected' => $esVersion ?? false,
+                'success' => $esVersion !== null
+                    && version_compare($esVersion, '6.9', '<')
+                    && version_compare($esVersion, '6.8', '>='),
+
+            ]
         ]));
 
         return $this;
