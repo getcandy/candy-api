f55bdbceb69654d971825cfe448f166a6b564761
Steve Hitchman <steve@builtbypxl.uk>
2020-10-22 15:32:32 +0100

    User resources

---
+++ src/Core/Users/Resources/UserDetailsResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace GetCandy\Api\Core\Users\Resources;
+
+use GetCandy\Api\Http\Resources\AbstractResource;
+
+class UserDetailsResource extends AbstractResource
+{
+    public function payload()
+    {
+        return [
+            'id' => $this->encoded_id,
+            'title' => $this->title,
+            'firstname' => $this->firstname,
+            'lastname' => $this->lastname,
+            'company_name' => $this->company_name,
+            'contact_number' => $this->contact_number,
+            'alt_contact_number' => $this->alt_contact_number,
+            'vat_no' => $this->vat_no,
+            'fields' => $this->fields,
+        ];
+    }
+}
