abe613dc36e466bb5e330a0488f271ecf9075b32
Alec <hello@itsalec.co.uk>
2021-03-10 12:13:57 +0000

    Add action to get current customer groups

---
+++ src/Core/Customers/Actions/FetchCurrentCustomerGroups.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace GetCandy\Api\Core\Customers\Actions;
+
+use GetCandy;
+use GetCandy\Api\Core\Scaffold\AbstractAction;
+use GetCandy\Api\Core\Customers\Models\CustomerGroup;
+use GetCandy\Api\Core\Customers\Actions\FetchDefaultCustomerGroup;
+use GetCandy\Api\Core\Customers\Resources\CustomerGroupCollection;
+
+class FetchCurrentCustomerGroups extends AbstractAction
+{
+    /**
+     * Determine if the user is authorized to make this action.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the action.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [];
+    }
+
+    /**
+     * Execute the action and return a result.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $user = $this->user();
+        $defaultGroup = FetchDefaultCustomerGroup::run();
+        $guestGroups = [$defaultGroup->id];
+        if (! $user) {
+            return $guestGroups;
+        }
+
+        $hubAccess = $user->hasAnyRole(['admin']);
+        
+        if (($hubAccess && GetCandy::isHubRequest()) ||
+            (! GetCandy::isHubRequest() && ! $hubAccess)
+        ) {
+            return $user->customer->customerGroups->pluck('id')->toArray();
+        }
+
+        return $guestGroups;
+    }
+}
