b14e2c3c60108641505cd50fbba17c519fc01b47
Alec Ritson <hello@itsalec.co.uk>
2021-08-16 11:51:34 +0100

    Bind user service to container

--- src/Providers/UserServiceProvider.php
+++ src/Providers/UserServiceProvider.php
@@ -3,6 +3,7 @@
 namespace GetCandy\Api\Providers;
 
 use GetCandy\Api\Core\Auth\Services\RoleService;
+use GetCandy\Api\Core\Auth\Services\UserService;
 use Illuminate\Support\ServiceProvider;
 
 class UserServiceProvider extends ServiceProvider
@@ -12,5 +13,10 @@ class UserServiceProvider extends ServiceProvider
         $this->app->singleton('getcandy.roles', function ($app) {
             return $app->make(RoleService::class);
         });
+
+        $this->app->bind('getcandy.users', function ($app) {
+            return $app->make(UserService::class);
+        });
+
     }
 }
