02944f48a7378cee94db74dcf7252964b210e4fe
Glenn Jacobs <glenn@neondigital.co.uk>
2024-07-01 12:22:15 +0100

    Update OrderFactory.php (#439)

--- src/Core/Orders/Factories/OrderFactory.php
+++ src/Core/Orders/Factories/OrderFactory.php
@@ -208,6 +208,8 @@ class OrderFactory implements OrderFactoryInterface
             $this->basket = $order->basket;
         }
 
+        DB::beginTransaction();
+
         if ($this->user) {
             $order->user()->associate($this->user);
             if ($order->wasRecentlyCreated) {
@@ -236,6 +238,8 @@ class OrderFactory implements OrderFactoryInterface
         if ($this->shipping) {
             $this->addShippingLine($order);
         }
+
+        DB::commit();
 
         event(new OrderSavedEvent($order, $this->basket));
 
